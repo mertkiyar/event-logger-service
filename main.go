22da4a501b16361b16d9004bbed60fdedc9b49ec
mertkiyar <mertkiyar69@gmail.com>
2026-08-24 14:49:57 +0300

    feat: create basic service with / endpoint

--- main.go
+++ main.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Event Logger Service working good!")
+	})
+
+	fmt.Println("Service working on 8080 port")
+
+	err := http.ListenAndServe(":8080", nil)
+
+	if err != nil {
+		fmt.Println("Service not started!", err)
+	}
 }
