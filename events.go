1001114f3b2ea9ee294ded610b9127d175e154b4
mertkiyar <mertkiyar69@gmail.com>
2026-08-25 16:22:23 +0300

    feat: change EventType from string to EventName to enforce strict event types

--- events.go
+++ events.go
@@ -1,6 +1,14 @@
 package main
 
+type EventName string
+
+const (
+	EventLogin  EventName = "login"
+	EventSignup EventName = "signup"
+	EventClick  EventName = "click"
+)
+
 type Event struct {
-	UserId    string `json:"user_id"`
-	EventType string `json:"event_type"`
+	UserId    string    `json:"user_id"`
+	EventType EventName `json:"event_type"`
 }
