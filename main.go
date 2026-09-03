a24747917644e0c17ba9214292f38ccb281781f1
mertkiyar <mertkiyar69@gmail.com>
2026-09-03 17:27:31 +0300

    feat: integrate rate limiter middleware to /events endpoint

--- main.go
+++ main.go
@@ -3,7 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
+	"sync"
+	"time"
 )
 
 var events []Event
@@ -16,13 +19,24 @@ func eventWorker() {
 	}
 }
 
+// mutex provides write data to map at the same time without concurrent map writes
+var (
+	visitors = make(map[string]*visitor)
+	mu       sync.Mutex
+)
+
+type visitor struct {
+	count    int
+	lastSeen time.Time
+}
+
 func main() {
 	go eventWorker()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintln(w, "Event Logger Service working good!")
 	})
 
-	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/events", rateLimiter(func(w http.ResponseWriter, r *http.Request) {
 		switch r.Method {
 		case http.MethodPost:
 			createEventHandler(w, r)
@@ -31,7 +45,7 @@ func main() {
 		default:
 			http.Error(w, "Method not allowed!", http.StatusMethodNotAllowed)
 		}
-	})
+	}))
 
 	http.HandleFunc("/stats", getStatsHandler)
 
@@ -112,6 +126,7 @@ func getEventsHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 }
+
 func getStatsHandler(w http.ResponseWriter, r *http.Request) {
 	// in this if block, the service allow only get method
 	if r.Method != http.MethodGet {
@@ -133,5 +148,47 @@ func getStatsHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(w, "Failed to encode stats!", http.StatusInternalServerError)
 		return
+	}
+}
+
+func rateLimiter(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		//excludes port from ip address
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err != nil {
+			http.Error(w, "Invalid IP address!", http.StatusBadRequest)
+			return
+		}
+
+		// lock mutex to safely access the visitors map
+		mu.Lock()
+		v, exists := visitors[ip]
+
+		// if visitor does not exist, create new visitor and get ip address
+		if !exists {
+			v = &visitor{count: 0, lastSeen: time.Now()}
+			visitors[ip] = v
+		}
+
+		// reset visit count if 10 seconds have passed since lastSeen
+		if time.Since(v.lastSeen) > time.Second*10 {
+			v.count = 0
+		}
+
+		// update lastSeen and increment visit count
+		v.lastSeen = time.Now()
+		v.count++
+
+		// if request count exceeds 100, throw an error
+		if v.count >= 100 {
+			mu.Unlock()
+			http.Error(w, "Too many requests", http.StatusTooManyRequests)
+			return
+		}
+		mu.Unlock()
+
+		//route the w and r value to create or get func
+		next(w, r)
 	}
 }
